a7fcab35191c4feff27cf7e16553b2b53cf6dba7
Eric <negggative@gmail.com>
2023-07-12 18:19:57 -0300

    Update CommandIgnore.java

--- src/main/java/games/negative/apexcore/command/ignore/CommandIgnore.java
+++ src/main/java/games/negative/apexcore/command/ignore/CommandIgnore.java
@@ -2,19 +2,12 @@ package games.negative.apexcore.command.ignore;
 
 import games.negative.alumina.command.Command;
 import games.negative.alumina.command.Context;
-import games.negative.apexcore.api.ApexAPI;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents the main command class for the /ignore command.
  */
 public class CommandIgnore implements Command {
-
-    private final ApexAPI api;
-
-    public CommandIgnore(@NotNull ApexAPI api) {
-        this.api = api;
-    }
 
     @Override
     public void execute(@NotNull Context context) {
