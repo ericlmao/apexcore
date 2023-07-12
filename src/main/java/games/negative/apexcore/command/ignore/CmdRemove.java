ab4b6630efc9074d247f1634b1f80ccfd1c411d1
Eric <negggative@gmail.com>
2023-07-12 18:53:41 -0300

    Add remove subcommand

--- src/main/java/games/negative/apexcore/command/ignore/CmdRemove.java
+++ src/main/java/games/negative/apexcore/command/ignore/CmdRemove.java
@@ -1,4 +1,50 @@
 package games.negative.apexcore.command.ignore;
 
-public class CmdRemove {
+import games.negative.alumina.command.Command;
+import games.negative.alumina.command.Context;
+import games.negative.apexcore.api.ApexAPI;
+import games.negative.apexcore.api.model.ApexPlayer;
+import games.negative.apexcore.core.Locale;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+/**
+ * Represents the "remove" subcommand for the /ignore command.
+ */
+public class CmdRemove implements Command {
+
+    private final ApexAPI api;
+
+    public CmdRemove(@NotNull ApexAPI api) {
+        this.api = api;
+    }
+
+    @Override
+    public void execute(@NotNull Context context) {
+        Player player = context.getPlayer();
+        assert player != null;
+
+        ApexPlayer user = api.getPlayer(player.getUniqueId());
+        if (user == null) {
+            Locale.GENERIC_PROFILE_ERROR.send(player);
+            return;
+        }
+
+        String[] args = context.args();
+        OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+
+        UUID uuid = target.getUniqueId();
+
+        if (!user.isIgnoring(uuid)) {
+            Locale.IGNORE_NOT_IGNORING.replace("%player%", target.getName()).send(player);
+            return;
+        }
+
+        user.removeIgnoredUser(uuid);
+        Locale.IGNORE_REMOVE_SINGLE.replace("%player%", target.getName()).send(player);
+    }
 }
