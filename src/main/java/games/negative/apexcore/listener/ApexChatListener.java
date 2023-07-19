e2385d7b7a0996b318329ed592659c18be9f8e6a
Negative <negggative@gmail.com>
2023-07-19 12:46:17 -0300

    Add bypass permission

--- src/main/java/games/negative/apexcore/listener/ApexChatListener.java
+++ src/main/java/games/negative/apexcore/listener/ApexChatListener.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -15,6 +17,8 @@ import java.util.Set;
 public class ApexChatListener implements Listener {
 
     private final ApexAPI api;
+
+    private static final Permission IGNORE_BYPASS = new Permission("apexcore.ignore.bypass", "Allows a player to bypass ignore restrictions.", PermissionDefault.OP);
 
     public ApexChatListener(@NotNull ApexAPI api) {
         this.api = api;
@@ -32,6 +36,7 @@ public class ApexChatListener implements Listener {
         Set<Player> recipients = event.getRecipients();
         for (Player recipient : recipients) {
             if (!user.isIgnoring(recipient.getUniqueId())) continue;
+            if (recipient.hasPermission(IGNORE_BYPASS)) continue;
 
             toRemove.add(recipient);
         }
