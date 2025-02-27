3939127132f13d5416fdb2a9636d44a1e7cdff01
Eric <negggative@gmail.com>
2025-02-26 20:18:46 -0400

    fix using wrong gson instance

--- src/main/java/games/negative/apexcore/core/provider/ApexDataManagerProvider.java
+++ src/main/java/games/negative/apexcore/core/provider/ApexDataManagerProvider.java
@@ -45,14 +45,12 @@ public class ApexDataManagerProvider implements ApexDataManager {
             return Maps.newHashMap();
         }
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-
         Map<UUID, ApexPlayer> players = Maps.newHashMap();
         for (File file : files) {
             if (!file.getName().endsWith(".json")) continue;
 
             try (Reader reader = new FileReader(file)) {
-                ApexPlayer player = gson.fromJson(reader, ApexPlayerImpl.class);
+                ApexPlayer player = GSON.fromJson(reader, ApexPlayerImpl.class);
                 players.put(player.getUniqueID(), player);
                 plugin.getLogger().info("Loaded player data from file " + file.getName() + "!");
             } catch (Exception e) {
@@ -78,8 +76,6 @@ public class ApexDataManagerProvider implements ApexDataManager {
             if (success) plugin.getLogger().info("Created data folder!");
         }
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-
         for (ApexPlayer player : players.values()) {
             UUID uuid = player.getUniqueID();
 
@@ -95,7 +91,7 @@ public class ApexDataManagerProvider implements ApexDataManager {
             }
 
             try (Writer writer = new FileWriter(file)) {
-                gson.toJson(player, writer);
+                GSON.toJson(player, writer);
                 plugin.getLogger().info("Saved player data to file " + file.getName() + "!");
             } catch (IOException e) {
                 plugin.getLogger().warning("Failed to save player data to file " + file.getName() + "!");
